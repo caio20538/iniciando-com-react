2407e22b0a5a617bf9f0c3d250a93ed9a9b3fb98
caio20538 <caio20538@gmail.com>
2025-07-09 18:48:31 -0300

    feat: create counter component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
 import React from "react";
 import './styles/app2.css'
 import Header from "./components/Header/header";
+import { Article } from "./components/Article/Article";
+import { Counter } from "./components/Counter/Counter";
 
 class App extends React.Component {
 
   render() {
     return(
-        <Header />
+      <>
+      <Header />
+
+      <Counter />
+      
+      {/*<section id="articles-list">
+        <Article title="titulo" provider="provedor"/>
+      </section>
+      */}
+      
+      </>
+        
     );
   }
 
