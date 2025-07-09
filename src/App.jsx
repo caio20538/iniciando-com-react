9d7794123781d61d0a2baa2d6ed1cf52e20e3dba
caio20538 <caio20538@gmail.com>
2025-07-09 17:20:06 -0300

    feat: initializing whith react

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import './styles/app2.css'
+import Header from "./components/Header/header";
+
+class App extends React.Component {
+
+  render() {
+    return(
+        <Header />
+    );
+  }
+
+}
+
+export default App;
