82163cfbc67abe27cd48c8bd6c9ff666b6b87238
caio20538 <caio20538@gmail.com>
2025-07-12 16:31:04 -0300

    refac: refactoring class to function and use useState

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,30 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-export class Counter extends React.Component{
+export function Counter (){
+    const [contador, setContador] = useState(0);
 
-    constructor(){
-        super();
-        //this.contador = 0;
-        this.state = {contador: 0};
-    }
 
-    render(){
-        return(
-            <div style={{marginTop: "20px", marginLeft: "20px"}}>
-                <h1>{this.state.contador}</h1>
+    return(
+        <div style={{marginTop: "20px", marginLeft: "20px"}}>
+            <h1>{contador}</h1>
 
-                <div>
-                    <button onClick={() => {
-                        //this.contador--;
-                        this.setState({contador: this.state.contador -1});
-                    }}>Diminuir</button>
+            <div>
+                <button onClick={() => {
+                    //this.contador--;
+                    setContador(contador - 1);
+                }}>Diminuir</button>
 
-                    <button onClick={() => {
-                        //this.contador++;
-                        this.setState({contador: this.state.contador +1});
-                    }}>Aumentar</button>
-                </div>
+                <button onClick={() => {
+                    //this.contador++;
+                    setContador(contador + 1);
+                }}>Aumentar</button>
             </div>
-        );
-    }
+        </div>
+    );
 }
