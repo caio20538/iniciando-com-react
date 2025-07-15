e9709279e13051e4e71ca1b1b9acb8be16d86d0f
caio20538 <caio20538@gmail.com>
2025-07-15 17:55:31 -0300

    feat: adicionando uma API para mostrar as notícias

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,60 @@
-import React from "react";
+import { useEffect, useState } from "react";
 import './styles/app2.css'
 import {Header} from "./components/Header/header";
 import { Article } from "./components/Article/Article";
-import { Counter } from "./components/Counter/Counter";
+import { ThreeDots } from "react-loader-spinner";
+//import { Counter } from "./components/Counter/Counter";
+import axios from "axios";
 
-class App extends React.Component {
+function App () {
 
-  render() {
-    return(
-      <>
-      {/* <Header />
+  const [news, setNews] = useState([]);
+
+  useEffect(() => {
+    async function loadNews() {
+      const response = await axios.get("https://api.spaceflightnewsapi.net/v4/articles/?format=json");
+      const newsData = response.data.results;
+
+      setNews(newsData);
+    }
+
+    loadNews();
+
+  }, []);
+
+  
+  return(
+    <>
+    <Header />
+    
+    <section id="articles-list">
+      {news.length === 0 ? (
+        <div style={{ height: '400px', width: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+          <ThreeDots 
+            height="80"
+            width='80'
+            color="white"
+          />
+        </div>
+      ) : news.map((article) => {
+        return(
+          <Article key={article.id}
+            title={article.title} 
+            provider={article.news_site}
+            description = {article.summary}
+            thumbnail = {article.image_url}
+            url = {article.url}
+          />
+        );
+      })}
+    </section>
+
+    {/* <Counter /> */}
+    
+    </>
       
-      <section id="articles-list">
-        <Article title="titulo" provider="provedor"/>
-      </section> */}
-
-      <Counter />
-      
-      </>
-        
-    );
-  }
+  );
+  
 
 }
 
--- src/components/Article/Article.jsx
+++ src/components/Article/Article.jsx
@@ -1,21 +1,18 @@
 import React from 'react';
-import articleImg from '../../assets/images/logo.jpg'
+//import articleImg from '../../assets/images/logo.jpg'
 import './style.css'
 
-export function Article({title, provider}){
+export function Article({title, provider, description, thumbnail, url}){
     return(
         <article id="article">
-            <img src={articleImg} alt="" />
+            <img src={thumbnail} alt="" />
 
             <div className="article-infos">
-                <h2>{title}</h2>
+                <h2><a href={url}>{title}</a></h2>
 
                 <h3>{provider}</h3>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
-                    Nobis aut reprehenderit quas officia, inventore iste expedita laborum 
-                    impedit assumenda repellendus consectetur harum illo soluta ratione laudantium ipsa, 
-                    enim reiciendis quidem!</p>
+                <p>{description}</p>
             </div>
         </article>
     );
